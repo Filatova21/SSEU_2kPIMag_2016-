df4438b8d9d17af7a6e307dd8c44647c7d7669b6
Filatova21 <flove63@mail.ru>
2016-10-20 20:45:45 +0400

    Лабораторная2.Самостоятельное задание 1: преобразование градусов Цельсия в градусы по Фаренгейту.

---
+++ Lab_2_primer1_dop1/Lab_2_primer1_dop1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_2_primer1_dop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a; //переменная,которую вводит пользователь 
+            int razn = 32;// разница Цельсия и Фаренгейта
+            int.TryParse(Console.ReadLine(), out a);
+            int itog;//переменная,выводящая итоговое значение градусов по Фаренгейту
+            itog = a + razn;
+            Console.WriteLine('Значение по Фаренгейту'+'='+itog);
+            Console.ReadKey();
+
+        }
+    }
+}
