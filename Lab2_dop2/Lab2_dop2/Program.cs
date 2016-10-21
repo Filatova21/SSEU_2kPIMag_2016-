c45a0b69ee8e71bc22e0595a031b5b940c725733
Filatova21 <flove63@mail.ru>
2016-10-21 17:05:13 +0400

    Лабораторная 2 дополнительное задание 2. Вычисление значения функции

---
+++ Lab2_dop2/Lab2_dop2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_dop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, x;
+            Console.WriteLine("Введите x");
+            int.TryParse(Console.ReadLine(), out x);
+            Console.WriteLine("Введите a");
+            int.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Введите b");
+            int.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("Введите c");
+            int.TryParse(Console.ReadLine(), out c);
+            double f;
+            f = -x / a * (Math.Sqrt(Math.Pow(x, 2) + b * x)) + c;
+            Console.WriteLine("f(x)" + "=" + f);
+            Console.ReadKey();
+        }
+    }
+}
