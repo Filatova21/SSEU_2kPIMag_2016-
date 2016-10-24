c46f72d95a4be1a4f7e7b7122f0d8624f9c60be9
Filatova21 <flove63@mail.ru>
2016-10-24 13:20:10 +0400

    Лабораторная 2,дополнительное задание 3(скидка на сумму от 1000 рублей)

---
+++ Lab_2_dop3/Lab_2_dop3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_dop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму покупки");
+            int sum;
+            int.TryParse(Console.ReadLine(), out sum);
+            double itog;
+            if (sum > 1000)
+            {
+                itog = sum - (sum * 15 / 1000);
+                Console.WriteLine("Сумма с учетом скидки составляет " + itog);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Cкидка на данную сумму не предоставляется");
+            };
+            Console.ReadKey();
+        }
+    }
+}
