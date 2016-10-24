203e87f06665c09acef16b0e16b46de88aa9e687
Filatova21 <flove63@mail.ru>
2016-10-24 15:44:14 +0400

    лабораторная 2.Доп.задание 4 (треугольник) часть 1.Равносторонний или равнобедренный

---
+++ Lab_2_dop4/Lab_2_dop4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_dop4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны треугольника");
+            float a, b, c;
+            if (float.TryParse(Console.ReadLine(), out a) && float.TryParse(Console.ReadLine(), out b) && float.TryParse(Console.ReadLine(), out c))
+            {
+                if (((a + b) > c) && ((a + c) > b) && ((b + c) > a))
+                {
+                    //проверка равносторонний ли треугольник
+                    if (a == b && a == c && b == c)
+                    {
+                        Console.WriteLine("Треугольник равносторонний");
+                    }
+                    else
+                    {
+                        // проверка равнобедренный ли треугольник
+                    if (a == b || a == c || b == c)
+                        {
+                        Console.WriteLine("Треугольник равнобедренный ");
+                        }
+                    };
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверное значение!Нужно ввести целое число");
+            }
+                Console.ReadKey();
+            }
+        }
+    }
+
