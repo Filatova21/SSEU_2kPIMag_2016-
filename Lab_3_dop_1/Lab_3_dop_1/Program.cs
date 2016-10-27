801382e41b7b721970c185595153761f5d5e2e0d
Filatova21 <flove63@mail.ru>
2016-10-27 17:20:40 +0400

    Лабораторная 3.Доп.задание 1(факториал)

---
+++ Lab_3_dop_1/Lab_3_dop_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3_dop_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Факториал какого числа необходимо посчитать?");
+            int N;
+            if (int.TryParse(Console.ReadLine(), out N))
+            {
+                int Proizv = 0;//факториал
+                int result = 1;
+                for (int i=1; i <= N;i++)
+                {
+                    result = result*i;
+                    i++;
+                }
+                //вывод результата
+                Console.WriteLine("Факториал числа " +N+" = "+ result);
+            };
+
+            //завершение программы
+            Console.ReadKey();
+        }
+    }
+}
+      
