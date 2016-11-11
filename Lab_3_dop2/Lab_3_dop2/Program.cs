2e8b148fc92c47c2312be21bc6ec03cf48d2c0df
Filatova21 <flove63@mail.ru>
2016-11-11 10:38:38 +0400

    Лабораторная 3. Доп.задание 2(сумму цифр и кол-во)

---
+++ Lab_3_dop2/Lab_3_dop2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_3_dop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int N;
+            int sum=0;
+            int kol=0;
+            if(int.TryParse(Console.ReadLine(),out N)){
+
+                while(N>0){
+                    sum = sum + N % 10;
+                    N=N/10;
+                    kol++;
+                }
+                Console.WriteLine("Количество цифр в числе = "+kol);
+                Console.WriteLine("Сумма цифр = "+sum);
+            }
+            else{
+                Console.WriteLine("Число введено некорректно! Введите целое число!");
+            };
+            Console.ReadKey();
+        }
+    }
+}
