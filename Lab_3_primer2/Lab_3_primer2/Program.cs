b79712e90cb839a8dd31aa5af23389933a207234
Filatova21 <flove63@mail.ru>
2016-11-11 10:45:07 +0400

    Л2.П2 Поиск максимального значения в массиве

---
+++ Lab_3_primer2/Lab_3_primer2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrayMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ввод длины массива
+            int n;
+            do
+            {
+                Console.Write("ВВедите длину массива: ");
+            } while (!int.TryParse(Console.ReadLine(), out n) || (n <= 0));
+            //инициализация массива
+            int[] a = new int[n];
+            int i = 0;
+            while (i < n)
+            {
+                Console.Write("Введите а[" + i + "]:");
+                if (int.TryParse(Console.ReadLine(), out a[i]))
+                {
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Введите целое число");
+                }; ;
+            };
+            //поиск максимального
+            int max = a[0];
+            for (i = 1; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                };
+            };
+            //вывод результата
+            Console.WriteLine("Максимальное значение: " + max);
+            //Выход из программы
+            Console.ReadKey();
+
+        }
+    }
+}
