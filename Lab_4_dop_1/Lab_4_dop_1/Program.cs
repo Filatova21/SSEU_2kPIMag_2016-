669b328bb57b873eb057a6e33fd40d378e8a2a7c
Filatova21 <flove63@mail.ru>
2016-11-25 10:30:21 +0400

    Лабораторная 4.Доп.задания 1(код клавиши)

---
+++ Lab_4_dop_1/Lab_4_dop_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_4_dop_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a;
+            Console.WriteLine("Нажмите клавишу");
+            char.TryParse(Console.ReadLine(), out a);
+            int b;
+            b = Convert.ToInt32(a);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
